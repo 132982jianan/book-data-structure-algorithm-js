a5d2d3028398b6be6db1cf6b234740132b65a346
jianan3 <jianan3@vipkid.com.cn>
2019-11-25 11:01:24 +0800

    14

--- 14-高级算法/Chap14-1.js
+++ 14-高级算法/Chap14-1.js
@@ -1,36 +1,44 @@
+// 递归
 function recurFib(n) {
-   if (n < 2) {
-      return n;
-   }
-   else {
-      return recurFib(n-1) + recurFib(n-2);
-   }
+    if (n < 2) {
+        return n;
+    } else {
+        return recurFib(n - 1) + recurFib(n - 2);
+    }
 }
 
+// 动态规划
 function dynFib(n) {
-   var val = [];
-   for (var i = 0; i <= n; ++i) {
-      val[i] = 0;
-   }
-   if (n == 1 || n == 2) {
-      return 1;
-   }
-   else {
-      val[1] = 1;
-      val[2] = 2;
-      for (var i = 3; i <= n; ++i) {
-         val[i] = val[i-1] + val[i-2];
-      }
-      return val[n-1];
-   }
+    var val = [];
+    for (var i = 0; i <= n; ++i) {
+        val[i] = 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        val[1] = 1;
+        val[2] = 2;
+        for (var i = 3; i <= n; ++i) {
+            val[i] = val[i - 1] + val[i - 2];
+        }
+        return val[n - 1];
+    }
 }
 
 var start = new Date().getTime();
-print(recurFib(10));
+console.log(recurFib(10));
 var stop = new Date().getTime();
-print("recursive time - " + (stop-start) + "milliseconds");
-print();
+console.log("recursive time=" + (stop - start) + " milliseconds", "\n");
+
 start = new Date().getTime();
-print(dynFib(10));
+console.log(dynFib(10));
 stop = new Date().getTime();
-print("dynamic programming time - " + (stop-start) + " milliseconds");
+console.log("dynamic programming time=" + (stop - start) + " milliseconds");
+
+/**
+ 55
+ recursive time=2 milliseconds
+
+ 55
+ dynamic programming time=0 milliseconds
+ */
